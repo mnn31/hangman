67b9c81ed2bd4df08cc4560b2809162ee0b69df8
mnn <>
2022-01-16 14:42:39 +0530

    Got wordlist from internet, picked random word from it.

--- hangman.py
+++ hangman.py
@@ -1,28 +1,36 @@
 
+import random
 
 
+DEBUG = False
+WORDS_FILENAME = "words.txt"
 
-def pick_word():
-
-    word = 'manan'
+def pick_word(wli):
+    word = random.choice(wli).lower().strip()
     return word
 
 
+def read_word_list(word_filename):
+    wordfile = open(word_filename, mode='r')
+    # read
+    wordlist = wordfile.readlines()
 
-
+    wordfile.close()
+    return wordlist
 
 
 def play(orig):
     lives = 10
     cur = ['?'] * len(orig)
+    print(cur)
     li_orig = list(orig)
     while (lives != 0 and cur != li_orig):
-        print(cur)
-        guess = input('Guess a letter: ')
+        guess = input('Guess a letter: ')[0]
         if guess in orig:
             for i in range(len(orig)):
                 if orig[i] == guess:
                     cur[i] = guess
+            print(cur)
 
         else:
             lives -= 1
@@ -32,15 +40,14 @@ def play(orig):
 
         
 
-    
-
-
-
-
 
 
 ## MAIN PROGRAM ##
-orig = pick_word()
+wli = read_word_list(WORDS_FILENAME)
+orig = pick_word(wli)
+if DEBUG:
+    print("Picked word is", orig)
+
 play(orig)
 
 
